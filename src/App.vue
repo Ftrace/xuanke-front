4f096e7ce8df44496d2c70abb740552dd3954f13
Ftrace <1577879915@qq.com>
2019-08-24 15:29:50 +0800

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,108 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <!-- <img src="./assets/logo.png">
+    <h1 v-for="(item,index) in list" :key="index">{{item}}-{{index}}</h1>
+
+
+    <input v-model="msg">
+    <button @click = "changeMsg">click me</button>
+    <hr/>
+    firstname: <input v-model="firstname" /> <br/>
+    lastname: <input v-model="lastname" /> <br/>
+    fullname: {{firstname}}&nbsp;{{lastname}} <br/>
+    fullname methods:{{getFullName()}} <br/>
+    fullname computed: {{fullname}} <br/>
+    fullname watch: {{fullnameWatch}} <br/>
+    <hr/>
+    <input v-model="name"/> <button @click="addUser">添加</button><br/>
+    <ol>
+      <li v-for="(item,index) in userList" :key="index">
+        <span v-if="index % 3 == 0" class="red">{{item}} - {{index}}</span>
+        <span v-else-if="index % 3 == 1" class="green">{{item}} - {{index}}</span>
+        <span v-else class="blue">{{item}} - {{index}}</span>
+      </li>
+    </ol> -->
+    <button-counter :prop-name="a">
+    <span>A</span>
+  </button-counter>
+    <button-counter prop-name="c"><span>B</span></button-counter>
+    <button-counter></button-counter>
+    <button-counter></button-counter>
+    <button-counter></button-counter>
+
+
+
+
   </div>
 </template>
 
 <script>
+import ButtonCounter from './components/ButtonCounter'
 export default {
   name: 'app',
+  components: {
+    ButtonCounter
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      flag: 0,
+      msg: 'Welcome to Your Vue.js App',
+      firstname:'fuyuhao',
+      lastname:'zhengxiuyan',
+      fullnameWatch:'fuyuhao zhengxiuyan',
+      list:[111,222,333],
+      name: '',
+      userList:[],
+      a:'A'
+    }
+  },
+
+  methods:{
+
+    addUser(){
+      this.userList.push(this.name)
+      this.name = ''
+    },
+    changeMsg(){
+      this.msg = "fyh"
+    },
+
+    getFullName(){
+      return this.firstname+' '+this.lastname
+    }
+  },
+
+  computed:{
+    fullname:function(){
+      return this.firstname+' '+this.lastname
+    }
+  },
+
+
+  watch: {
+    firstname: function(val){
+      this.fullnameWatch = val + ' ' +this.lastname
+    },
+
+    lastname: function(val){
+      this.fullnameWatch = this.firstname+' '+val
     }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.red{
+  background-color: red;
+
+}
+.green{
+  background-color: green;
+
+}
+.blue{
+  background-color: blue;
+
 }
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
---
+++ src/components/ButtonCounter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="button-counter">
+
+
+    <slot ></slot>
+    <button @click = "count++">{{propName}}点击了我{{count}}次</button>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'button-counter',
+  props:['propName'],
+  data () {
+    return {
+
+    count:0
+    }
+  },
+
+  methods:{
+
+
+  }
+}
+</script>
+
+<style lang="scss">
+
+
+</style>
